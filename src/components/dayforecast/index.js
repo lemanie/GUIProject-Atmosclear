bbe12dccea2bec19d7e3174b7c84d0cfdff4870a
lgh <lgh@lgh>
2017-02-11 23:54:50 +0000

    added comments to dayforecast and weekforecast

--- src/components/dayforecast/index.js
+++ src/components/dayforecast/index.js
@@ -5,42 +5,42 @@ export default class DayForecast extends Component {
 	render() {
 		return (
 			<div class={style.dayForecast}>
-				<div class={"row " + style.headerrow}>
-				  <div class={style.block + " " + style.col1}>
-
-				  </div>
-				  <div class={style.block + " " + style.col2}>
-					<i class={"wi wi-strong-wind " + style.icon}></i>
-					<span class={style.blockData}>Wind Speed</span>
-				  </div>
-				  <div class={style.block + " " + style.col3}>
-					<i class={"wi wi-rain " + style.icon}></i>
-					<span class={style.blockData}>Chance of Rain</span>
-				  </div>
-				  <div class={style.block + " " + style.col4}>
-					<i class={"wi wi-cloud " + style.icon}></i>
-					<span class={style.blockData}>Cloud Coverage</span>
-				  </div>
-				  <div class={style.block + " " + style.col5}>
-					<i class={"wi wi-dust " + style.icon}></i>
-					<span class={style.blockData}>Visibility Level</span>
-				  </div>
+				<div class={"row " + style.headerrow}> {/*this row is the headerrow*/}
+					<div class={style.block + " " + style.col1}>
+						{/*Empty first (top left) cell*/}
+					</div>
+					<div class={style.block + " " + style.col2}> {/*header cell with the wind icon and description*/}
+						<i class={"wi wi-strong-wind " + style.icon}></i>
+						<span class={style.blockData}>Wind Speed</span>
+					</div>
+					<div class={style.block + " " + style.col3}> {/*header cell with the rain icon and description*/}
+						<i class={"wi wi-rain " + style.icon}></i>
+						<span class={style.blockData}>Chance of Rain</span>
+					</div>
+					<div class={style.block + " " + style.col4}> {/*header cell with the cloud icon and description*/}
+						<i class={"wi wi-cloud " + style.icon}></i>
+						<span class={style.blockData}>Cloud Coverage</span>
+					</div>
+					<div class={style.block + " " + style.col5}> {/*header cell with the dust icon and description*/}
+						<i class={"wi wi-dust " + style.icon}></i>
+						<span class={style.blockData}>Visibility Level</span>
+					</div>
 				</div>
-				<div class={"row " + style.bodyrow}>
+				<div class={"row " + style.bodyrow}> {/*wrap the body row*/}
 				  <div class={style.block + " " + style.col1}>
-					16:00
+						16:00 {/*hour of the day*/}
 				  </div>
 				  <div class={style.block + " " + style.col2}>
-					7mph
+						7mph {/*data for wind speed*/}
 				  </div>
 				  <div class={style.block + " " + style.col3}>
-					60%
+						60% {/*data for rain*/}
 				  </div>
 				  <div class={style.block + " " + style.col4}>
-					70%
+						70% {/*data for cloud coverage*/}
 				  </div>
 				  <div class={style.block + " " + style.col5}>
-					70%
+						70% {/*data for visibility*/}
 				  </div>
 				</div>
 				<div class={"row " + style.bodyrow}>
--- src/components/weekforecast/index.js
+++ src/components/weekforecast/index.js
@@ -4,61 +4,61 @@ import style from './style';
 export default class WeekForecast extends Component {
 	render() {
 		return (
-			<div class={style.dayForecast}>
-				<div class={"row " + style.headerrow}>
+			<div class={style.weekForecast}> {/*wrap the table around one div*/}
+				<div class={"row " + style.headerrow}> {/*this row is the headerrow*/}
 				  <div class={style.block + " " + style.col1}>
-					
+						{/*Empty first (top left) cell*/}
 				  </div>
-				  <div class={style.block + " " + style.col2}>
-					<i class={"wi wi-strong-wind " + style.icon}></i>
-					<span class={style.blockData}>Wind Speed</span>
+				  <div class={style.block + " " + style.col2}> {/*header cell with the wind icon and description*/}
+						<i class={"wi wi-strong-wind " + style.icon}></i>
+						<span class={style.blockData}>Wind Speed</span>
 				  </div>
-				  <div class={style.block + " " + style.col3}>
-					<i class={"wi wi-rain " + style.icon}></i>
-					<span class={style.blockData}>Chance of Rain</span>
+				  <div class={style.block + " " + style.col3}> {/*header cell with the rain icon and description*/}
+						<i class={"wi wi-rain " + style.icon}></i>
+						<span class={style.blockData}>Chance of Rain</span>
 				  </div>
-				  <div class={style.block + " " + style.col4}>
-					<i class={"wi wi-cloud " + style.icon}></i>
-					<span class={style.blockData}>Cloud Coverage</span>
+				  <div class={style.block + " " + style.col4}> {/*header cell with the cloud icon and description*/}
+						<i class={"wi wi-cloud " + style.icon}></i>
+						<span class={style.blockData}>Cloud Coverage</span>
 				  </div>
-				  <div class={style.block + " " + style.col5}>
-					<i class={"wi wi-dust " + style.icon}></i>
-					<span class={style.blockData}>Visibility Level</span>
+				  <div class={style.block + " " + style.col5}> {/*header cell with the dust icon and description*/}
+						<i class={"wi wi-dust " + style.icon}></i>
+						<span class={style.blockData}>Visibility Level</span>
 				  </div>
 				</div>
-				<div class={"row " + style.bodyrow}>
+				<div class={"row " + style.bodyrow}> {/*wrap the body row*/}
 				  <div class={style.block + " " + style.col1}>
-					Monday
+						Monday {/*day of the week*/}
 				  </div>
 				  <div class={style.block + " " + style.col2}>
-					7mph
+						7mph {/*data for wind speed*/}
 				  </div>
 				  <div class={style.block + " " + style.col3}>
-					60%
+						60% {/*data for rain*/}
 				  </div>
 				  <div class={style.block + " " + style.col4}>
-					70%
+						70% {/*data for cloud coverage*/}
 				  </div>
 				  <div class={style.block + " " + style.col5}>
-					70%
-				  </div>			  
+						70% {/*data for visibility*/}
+				  </div>
 				</div>
-				<div class={"row " + style.bodyrow}>
+				<div class={"row " + style.bodyrow}> {/*This is a copy of the above*/}
 				  <div class={style.block + " " + style.col1}>
-					Tuesday
+						Tuesday
 				  </div>
 				  <div class={style.block + " " + style.col2}>
-					7mph
+						7mph
 				  </div>
 				  <div class={style.block + " " + style.col3}>
-					60%
+						60%
 				  </div>
 				  <div class={style.block + " " + style.col4}>
-					70%
+						70%
 				  </div>
 				  <div class={style.block + " " + style.col5}>
-					70%
-				  </div>			  
+						70%
+				  </div>
 				</div>
 				<div class={"row " + style.bodyrow}>
 				  <div class={style.block + " " + style.col1}>
@@ -75,7 +75,7 @@ export default class WeekForecast extends Component {
 				  </div>
 				  <div class={style.block + " " + style.col5}>
 					70%
-				  </div>			  
+				  </div>
 				</div>
 				<div class={"row " + style.bodyrow}>
 				  <div class={style.block + " " + style.col1}>
@@ -92,7 +92,7 @@ export default class WeekForecast extends Component {
 				  </div>
 				  <div class={style.block + " " + style.col5}>
 					70%
-				  </div>			  
+				  </div>
 				</div>
 				<div class={"row " + style.bodyrow}>
 				  <div class={style.block + " " + style.col1}>
@@ -109,7 +109,7 @@ export default class WeekForecast extends Component {
 				  </div>
 				  <div class={style.block + " " + style.col5}>
 					70%
-				  </div>			  
+				  </div>
 				</div>
 				<div class={"row " + style.bodyrow}>
 				  <div class={style.block + " " + style.col1}>
@@ -126,7 +126,7 @@ export default class WeekForecast extends Component {
 				  </div>
 				  <div class={style.block + " " + style.col5}>
 					70%
-				  </div>			  
+				  </div>
 				</div>
 				<div class={"row " + style.bodyrow}>
 				  <div class={style.block + " " + style.col1}>
@@ -143,7 +143,7 @@ export default class WeekForecast extends Component {
 				  </div>
 				  <div class={style.block + " " + style.col5}>
 					70%
-				  </div>			  
+				  </div>
 				</div>
 			</div>
 		);
